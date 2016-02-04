23dafcd68f0861e5b9849134dbfb0ad5f4153145
schiesla <schiesla@rose-hulman.edu>
2016-02-04 18:52:41 -0500

    added fields and getters to POI

--- src/PointofInterest.java
+++ src/PointofInterest.java
@@ -6,13 +6,17 @@ public class PointofInterest {
 	String type;
 	double straightLineDist;
 	double rating;
+	double lat;
+	double longit;
 	ArrayList<PointofInterest> neighbors;	
 	
-	public PointofInterest(String name, String type){
+	public PointofInterest(String name, String type, double latitude, double longitude){
 		this.name = name;
 		this.type = type;
 		this.straightLineDist = 0.0;
 		this.rating = 0.0;
+		this.lat = latitude;
+		this.longit = longitude;
 		this.neighbors = new ArrayList<PointofInterest>();	
 	}
 	
@@ -32,6 +36,13 @@ public class PointofInterest {
 		return this.rating;
 	}
 	
+	public double getLatitude(){
+		return this.lat;
+	}
+	
+	public double getLongitude(){
+		return this.longit;
+	}
 	public ArrayList<PointofInterest> getNeighbors(){
 		return this.neighbors;
 	}
