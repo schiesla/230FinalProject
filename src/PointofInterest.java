4df78d0db6ac5ac44b556c6f098f8af5936cfea8
schiesla <schiesla@rose-hulman.edu>
2016-02-08 12:35:16 -0500

    added distance formula

--- src/PointofInterest.java
+++ src/PointofInterest.java
@@ -8,6 +8,7 @@ public class PointofInterest {
 	double rating;
 	double lat;
 	double longit;
+	double radOfEarth;
 	ArrayList<PointofInterest> neighbors;	
 	
 	public PointofInterest(String name, String type, double latitude, double longitude, double rating){
@@ -17,6 +18,7 @@ public class PointofInterest {
 		this.rating = rating;
 		this.lat = latitude;
 		this.longit = longitude;
+		this.radOfEarth = 3959;
 		this.neighbors = new ArrayList<PointofInterest>();	
 	}
 	
@@ -34,6 +36,26 @@ public class PointofInterest {
 	
 	public double getRating(){
 		return this.rating;
+	}
+	/**
+	 * 
+	 * takes the lat and lon of two Points of Interest and calculates the distance in miles 
+	 * between the two places. Formula aquired from https://en.wikipedia.org/wiki/Haversine_formula
+	 *
+	 * @param neighbor
+	 * @return distance in miles
+	 */
+	public double DistToNeighbor(PointofInterest neighbor){
+		double latTwo = ((neighbor.lat * 3.14)/180.0);
+		double latOne = ((this.lat * 3.14)/180.0);
+		double longTwo = ((neighbor.longit * 3.14)/180.0);
+		double longOne = ((this.longit * 3.14)/180.0);
+		double dlong = longTwo - longOne;
+		double dlat =  latTwo - latOne;
+		double a = Math.pow((Math.sin(dlat/2)), 2) + Math.cos(latOne) * Math.cos(latTwo) * Math.pow(Math.sin(dlong/2), 2);
+		double d = 2 * this.radOfEarth * Math.asin(Math.sqrt(a));
+		return d;
+		
 	}
 	
 	public double getLatitude(){
