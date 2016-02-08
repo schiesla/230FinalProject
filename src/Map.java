aee726e57bdc68de452ebb5b33e6c5f92571f94c
atyderada <anthonyderada@gmail.com>
2016-02-08 11:37:40 -0500

    text input

--- src/Map.java
+++ src/Map.java
@@ -1,4 +1,6 @@
+import java.io.File;
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 public class Map {
@@ -12,6 +14,21 @@ public class Map {
 	
 	private void populateMap() {
 		
+		Scanner input = new Scanner((Readable) new File("text"));
+		while (input.hasNextLine()) {
+			String row = input.nextLine();
+			for (int gridX = 0; gridX < row.length(); gridX++) {
+				if (row.charAt(gridX) == 'C') {
+					//
+				}
+				if (row.charAt(gridX) == 'M') {
+					//
+				}
+			}
+			// increase y
+		}
+
+		input.close();
 	}
 	
 }
