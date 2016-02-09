3f4518dd55b1caadaf72e87e624ce4bd420614cd
wlucas7474 <weierlj@rose-hulman.edu>
2016-02-09 17:38:27 -0500

    Have cities being drawn on the map

--- src/MapDrawer.java
+++ src/MapDrawer.java
@@ -30,11 +30,18 @@ public class MapDrawer extends JComponent {
 	private static final double BOTTOM_RIGHT_CORNER_LONG = 102.0421;
 	private static final Point2D.Double BOTTOM_RIGHT_CORNER = new Point2D.Double(BOTTOM_RIGHT_CORNER_LONG, BOTTOM_RIGHT_CORNER_LAT);
 	
-//	private static final double CENTER_LAT = 39.0000;
-//	private static final double CENTER_LONG = 105.5000;
+	private static final double CENTER_LAT = (TOP_LEFT_CORNER_LAT + BOTTOM_RIGHT_CORNER_LAT)/2.0;
+	private static final double CENTER_LONG = (TOP_LEFT_CORNER_LONG + BOTTOM_RIGHT_CORNER_LONG)/2.0;
+	
 	private static final double WIDTH = TOP_LEFT_CORNER.x - BOTTOM_RIGHT_CORNER.x;
 	private static final double HEIGHT = TOP_LEFT_CORNER.y - BOTTOM_RIGHT_CORNER.y;
 	private static final Dimension d = new Dimension();
+	
+	private static final double FRAME_MULTIPLIER = 100;
+	private static final double MAP_RADIUS = 10;
+	
+	private static final double FRAME_WIDTH = WIDTH * FRAME_MULTIPLIER;
+	private static final double FRAME_HEIGHT = HEIGHT * FRAME_MULTIPLIER;
 	
 	public static void main(String[] args) {
 		PointofInterest p1 = new PointofInterest("Vali Ski Resort", "Resort", 39.6391, 106.3738, 2.5);
@@ -42,13 +49,14 @@ public class MapDrawer extends JComponent {
 		HashMap<String, PointofInterest> map = new HashMap<String, PointofInterest>();
 		map.put("Vali Ski Resort", p1);
 		map.put("Rocky Mountain National Park", p2);
-		MapDrawer md = new MapDrawer(map);
+		JFrame frame = new JFrame();
+		MapDrawer md = new MapDrawer(frame, map);
 	}
 	
-	public MapDrawer(HashMap<String, PointofInterest> map) {
+	public MapDrawer(JFrame frame, HashMap<String, PointofInterest> map) {
 		this.map = map;
 		JFrame f = new JFrame();
-		this.d.setSize(WIDTH * 100, HEIGHT * 100);
+		this.d.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 		f.setSize(this.d);
 		f.add(this);
 		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
@@ -62,12 +70,12 @@ public class MapDrawer extends JComponent {
 	}
 	
 	public void drawMap(Graphics2D g) {
-		g.translate(WIDTH/2, HEIGHT/2);
+		g.translate(FRAME_WIDTH/2.0, FRAME_HEIGHT/2.0);
 		for (String s : this.map.keySet()) {
 			double lat = this.map.get(s).getLatitude();
 			double longit = this.map.get(s).getLongitude();
-			Point2D.Double point = new Point2D.Double(longit - WIDTH/2, lat - HEIGHT/2);
-			Rectangle2D.Double rect = new Rectangle2D.Double(point.x, point.y, 10, 10);
+			Point2D.Double point = new Point2D.Double(longit - CENTER_LONG, lat - CENTER_LAT);
+			Rectangle2D.Double rect = new Rectangle2D.Double(point.x * FRAME_MULTIPLIER - MAP_RADIUS/2.0, point.y * FRAME_MULTIPLIER - MAP_RADIUS/2.0, MAP_RADIUS, MAP_RADIUS);
 			g.setColor(Color.BLACK);
 			g.fill(rect);
 		}
