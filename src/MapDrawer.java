3ba94dea985b396c7ff14aaae191e386c307c149
wlucas7474 <weierlj@rose-hulman.edu>
2016-02-09 12:32:22 -0500

    Added frame size based on Colorado coordinates

--- src/MapDrawer.java
+++ src/MapDrawer.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
@@ -12,15 +13,34 @@ import javax.swing.JFrame;
 public class MapDrawer extends JComponent {
 	
 	private HashMap<String, PointofInterest> map;
-	private static final double CENTER_LAT = 39.0000;
-	private static final double CENTER_LONG = 105.5000;
-	private static final int WIDTH = 1200;
-	private static final int HEIGHT = 1000;
+	
+	private static final double TOP_LEFT_CORNER_LAT = 41.0007;
+	private static final double TOP_LEFT_CORNER_LONG = 109.0501;
+	private static final Point2D.Double TOP_LEFT_CORNER = new Point2D.Double(TOP_LEFT_CORNER_LONG, TOP_LEFT_CORNER_LAT);
+	
+	private static final double TOP_RIGHT_CORNER_LAT = 41.0024;
+	private static final double TOP_RIGHT_CORNER_LONG = 102.0517;
+	private static final Point2D.Double TOP_RIGHT_CORNER = new Point2D.Double(TOP_RIGHT_CORNER_LONG, TOP_RIGHT_CORNER_LAT);
+	
+	private static final double BOTTOM_LEFT_CORNER_LAT = 36.9991;
+	private static final double BOTTOM_LEFT_CORNER_LONG = 109.0452;
+	private static final Point2D.Double BOTTOM_LEFT_CORNER = new Point2D.Double(BOTTOM_LEFT_CORNER_LONG, BOTTOM_LEFT_CORNER_LAT);
+	
+	private static final double BOTTOM_RIGHT_CORNER_LAT = 36.9930;
+	private static final double BOTTOM_RIGHT_CORNER_LONG = 102.0421;
+	private static final Point2D.Double BOTTOM_RIGHT_CORNER = new Point2D.Double(BOTTOM_RIGHT_CORNER_LONG, BOTTOM_RIGHT_CORNER_LAT);
+	
+//	private static final double CENTER_LAT = 39.0000;
+//	private static final double CENTER_LONG = 105.5000;
+	private static final double WIDTH = TOP_LEFT_CORNER.x - BOTTOM_RIGHT_CORNER.x;
+	private static final double HEIGHT = TOP_LEFT_CORNER.y - BOTTOM_RIGHT_CORNER.y;
+	private static final Dimension d = new Dimension();
 	
 	public MapDrawer(HashMap<String, PointofInterest> map) {
 		this.map = map;
 		JFrame f = new JFrame();
-		f.setSize(WIDTH, HEIGHT);
+		this.d.setSize(WIDTH, HEIGHT);
+		f.setSize(this.d);
 		f.add(this);
 		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 		f.setVisible(true);
@@ -37,7 +57,7 @@ public class MapDrawer extends JComponent {
 		for (String s : this.map.keySet()) {
 			double lat = this.map.get(s).getLatitude();
 			double longit = this.map.get(s).getLongitude();
-			Point2D.Double point = new Point2D.Double(longit - CENTER_LONG, lat - CENTER_LAT);
+			Point2D.Double point = new Point2D.Double(longit - WIDTH/2, lat - HEIGHT/2);
 			Rectangle2D.Double rect = new Rectangle2D.Double(point.x, point.y, 3, 3);
 			g.setColor(Color.BLACK);
 			g.fill(rect);
