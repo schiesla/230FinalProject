372156249305ee8a185a632ded66a430cf371793
wlucas7474 <weierlj@rose-hulman.edu>
2016-02-12 03:56:06 -0500

    MouseListener works for clicking on cities, corresponds with FRAME_MULTIPLIER, as does everything else. Map can change sizes WOO!!!

--- src/MapDrawer.java
+++ src/MapDrawer.java
@@ -39,42 +39,18 @@ public class MapDrawer extends JPanel implements MouseListener {
 	private static final Dimension d = new Dimension();
 	
 	private static final double FRAME_MULTIPLIER = 100;
-	private static final double MAP_RADIUS = 10;
+	private static final double MAP_RADIUS = FRAME_MULTIPLIER / 10;
 	
 	private static final double FRAME_WIDTH = WIDTH * FRAME_MULTIPLIER;
 	private static final double FRAME_HEIGHT = HEIGHT * FRAME_MULTIPLIER;
 	
 	private HashMap<Shape, Map.PointofInterest> shapes = new HashMap<Shape, Map.PointofInterest>();
-	
-//	public static void main(String[] args) {
-//		PointofInterest p1 = new PointofInterest("Vali Ski Resort", "Resort", 39.6391, 106.3738, 2.5);
-//		PointofInterest p2 = new PointofInterest("Rocky Mountain National Park", "Park", 40.3333, 105.7089, 2.6);
-//		PointofInterest p3 = new PointofInterest("Pikes Peak", "Park", 38.8405, 105.0442, 3.5);
-//		PointofInterest p4 = new PointofInterest("Mount Evans", "Park", 39.5883, 105.6438, 4.1);
-//		PointofInterest p5 = new PointofInterest("Canyons of the Ancients National Park", "Park", 37.3706, 109.0000, 1.9);
-//		PointofInterest p6 = new PointofInterest("Copper Mountain", "Park", 39.5017, 106.1564, 3.1);
-//		PointofInterest p7 = new PointofInterest("Denver", "City", 39.7392, 104.9903, 4.20);
-//		HashMap<String, PointofInterest> map = new HashMap<String, PointofInterest>();
-//		map.put("Vali Ski Resort", p1);
-//		map.put("Rocky Mountain National Park", p2);
-//		map.put("Pikes Peak", p3);
-//		map.put("Mount Evans", p4);
-//		map.put("Canyons of the Ancients National Park", p5);
-//		map.put("Copper Mountain", p6);
-//		map.put("Denver", p7);
-//		JFrame frame = new JFrame();
-//		MapDrawer md = new MapDrawer(frame, map);
-//	}
-	
+		
 	public MapDrawer(HashMap<String, Map.PointofInterest> map) {
 
-//		JButton hi = new JButton("test");
-//		this.panel.add(hi);
 		this.map = map;
 		this.d.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 		setPreferredSize(this.d);
-//		f.setSize(this.d);
-//		f.add(this);
 		this.addMouseListener(this);
 	}
 	
@@ -89,13 +65,12 @@ public class MapDrawer extends JPanel implements MouseListener {
 		Rectangle2D.Double outline = new Rectangle2D.Double(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
 		g.draw(outline);
 		g.translate(FRAME_WIDTH/2.0, FRAME_HEIGHT/2.0);
-		g.rotate(Math.toRadians(180));
 		for (Object s : this.map.keySet()) {
 			String type = this.map.get(s).getType();
 			double lat = this.map.get(s).getLatitude();
 			double longit = this.map.get(s).getLongitude();
 			Point2D.Double point = new Point2D.Double(longit - CENTER_LONG, lat - CENTER_LAT);
-			Rectangle2D.Double rect = new Rectangle2D.Double(point.x * FRAME_MULTIPLIER - MAP_RADIUS/2.0, point.y * FRAME_MULTIPLIER - MAP_RADIUS/2.0, MAP_RADIUS, MAP_RADIUS);
+			Rectangle2D.Double rect = new Rectangle2D.Double(point.x * -FRAME_MULTIPLIER - MAP_RADIUS/2.0, point.y * -FRAME_MULTIPLIER - MAP_RADIUS/2.0, MAP_RADIUS, MAP_RADIUS);
 			this.shapes.put(rect, this.map.get(s));
 			System.out.println(this.map.get(s).getName() + ": " + rect.x + " " + rect.y);
 			if (type.equals("City"))
@@ -106,7 +81,6 @@ public class MapDrawer extends JPanel implements MouseListener {
 				g.setColor(Color.GREEN);
 			g.fill(rect);
 		}
-		g.rotate(-Math.toRadians(180));
 		g.translate(-FRAME_WIDTH/2.0, -FRAME_HEIGHT/2.0);
 	}
 	
@@ -118,11 +92,14 @@ public class MapDrawer extends JPanel implements MouseListener {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		System.out.println("Should print");
-		System.out.println(e.getX() + "");
-		System.out.println(e.getY() + "");
+//		System.out.println("Should print");
+		double mouseX = e.getX() - FRAME_WIDTH/2.0 - MAP_RADIUS/2.0;
+		double mouseY = e.getY() - FRAME_HEIGHT/2.0 - MAP_RADIUS/2.0;
 		for(Shape s : this.shapes.keySet()) {
-			if (s.contains((e.getX() - CENTER_LONG) * FRAME_MULTIPLIER, (e.getY() - CENTER_LAT) * FRAME_MULTIPLIER)) {
+			double exs = Math.pow(mouseX - s.getBounds2D().getX(), 2);
+			double whys = Math.pow(mouseY - s.getBounds2D().getY(), 2);
+			double distForm = Math.sqrt(exs + whys);
+			if (distForm < MAP_RADIUS/2.0) {
 				System.out.println(this.shapes.get(s).getName());
 			}
 		}
